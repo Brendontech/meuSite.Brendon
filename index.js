0eb4a39415a6504ba58c54df71835382ae54d3fe
Brendon33 <fernandesbrendon83@gmail.com>
2024-02-05 17:25:51 -0300

    versão 1.3

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function toggleDescription(descriptionId) {
         if (conhecimentosHeight > windowHeight) {
             conhecimentosContainer.style.height = '100vh';
             conhecimentosContainer.style.overflowY = 'auto';
+            conhecimentosContainer.style.scrollbarColor = '#1D1D30 #ffffff';
         }
 
         // Garante que a altura do .conhecimentos não ultrapasse 100vh
@@ -41,12 +42,14 @@ function toggleDescription(descriptionId) {
         if (conhecimentosInnerHeight > windowHeight) {
             conhecimentosInnerContainer.style.height = '100vh';
             conhecimentosInnerContainer.style.overflowY = 'auto';
+            conhecimentosInnerContainer.style.scrollbarColor = ' #a5f767 #1D1D30';
         }
     } else {
         // Se o bloco clicado estava visível, fecha todos
         allDescriptionContainers.forEach(function (container) {
             container.style.height = ''; // Limpa a altura para voltar ao normal
             container.style.overflowY = ''; // Limpa o overflowY
+            container.style.scrollbarColor = ''; // Limpa a cor da barra de rolagem
         });
 
         // Restaura a altura do #Conhecimentos
